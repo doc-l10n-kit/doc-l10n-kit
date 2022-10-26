56b8aa3cb91cb87a4f5d8da72549558161cf544b
Yoshikazu Nojima <mail@ynojima.net>
2022-10-26 11:14:45 +0900

    Parameterize kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.allopen") version "1.6.21"
+    kotlin("jvm")
+    kotlin("plugin.allopen")
     id("io.quarkus")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 pluginManagement {
+    val kotlinVersion: String by settings
     val quarkusPlatformVersion: String by settings
     plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.allopen") version kotlinVersion
         id("io.quarkus") version quarkusPlatformVersion
     }
     repositories {
